ee6d782b7dce45c37956c124af463eb82c48d0c2
shubhra-1337 <shubhra1337@gmail.com>
2026-01-19 05:38:10 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,18 @@
 import express from "express";
+import connectDB from "../config/db.js";
 import mbtiRoutes from "../routes/mbtiRoutes.js";
 import visitorRoutes from "../routes/visitors.js";
-import connectDB from "../config/db.js";
 
 const app = express();
 
 app.use(express.json());
 
+// Connect to MongoDB
 await connectDB();
 
+// Routes
 app.use("/api/mbti", mbtiRoutes);
 app.use("/api/visitors", visitorRoutes);
 
-export default app; // 🔥 THIS is the key
+// Export for Vercel
+export default app;
